4ae449f911e6aab489851c4cd5eb5b53ad1ba17d
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-21 12:28:42 +0530

    Add Functionality to make the Express Server Start Listening as the Connection to MongoDB is successful

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({path: "./env"})
-connectDB();
+
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 8000;
+        app.listen(PORT, () => {
+            console.log(`Server is listening on Port: ${PORT}`);
+        });
+    })
+    .catch(err => console.error("MongoDB Connection Failed!!!"));
 
 
 
