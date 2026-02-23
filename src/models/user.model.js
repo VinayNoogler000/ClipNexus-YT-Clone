f9daf61202661b92aa5444780b256edcbde97fdc
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-23 18:31:27 +0530

    Add custome methods to generate access and refresh tokens in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import {Schema, Model} from "mongoose";
 import brcypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema({
     userName: {
@@ -62,6 +63,27 @@ userSchema.methods.isPasswordCorrect = async function(password) {
     catch(err) {
         console.error("Error in Comparing User-Entered-Pass with Encrypted Password: \n", err);
     }
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        }, 
+        process.env.ACCESS_TOKEN_SECRET, 
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        { _id: this._id },
+        process.env.REFRESH_TOKEN_SECRET, 
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
 }
 
 export const User = Model("User", userSchema);
