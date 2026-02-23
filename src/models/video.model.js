76cdd85e3e33a531671ac83374e8c2c24cf7a93b
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-23 17:44:59 +0530

    Create Video Model

---
+++ src/models/video.model.js
@@ -0,0 +1,39 @@
+import {Schema, Model} from "mongoose";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, // Cloudinary URL
+        required: true
+    },
+    thumbnail: {
+        type: String, // Cloudinary URL
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number, // Cloudinary URL
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, {timestamps: true});
+
+export const Video = Model("Video", videoSchema);
