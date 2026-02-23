ce36c415fc4f94dee578c804d4a99ba1d3f1a03b
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-23 17:38:36 +0530

    Add User model

---
+++ src/models/user.model.js
@@ -0,0 +1,45 @@
+import {Schema, Model} from "mongoose";
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, // Cloudinary URL
+        required: true,
+    },
+    coverImage: {
+        type: String, // Cloudinary URL
+    },
+    watchHistory: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+    },
+    refreshToken: {
+        type: String,
+    }
+}, {timestamps: true});
+
+export const User = Model("User", userSchema);
