3a957d471e40656fc786a59112fc2418e84c772b
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-26 21:06:39 +0530

    Removed preflightContinue and optionsSuccessStatus from CORS configuration, cuz they were unnecessary

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
-    preflightContinue: true,
-    optionsSuccessStatus: 204
 }));
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit:"16kb"}));
