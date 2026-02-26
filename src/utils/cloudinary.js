59619cb37cf556ffe39f324d3594018c5f97f000
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-26 19:36:08 +0530

    Added an utility file for configuring Cloudinary and using it's to upload images/videos files to the cloud.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+});
+
+// Function to Uploads a image/video file
+const uploadAssetToCloudinary = async (filePath) => {
+    try {
+        if (!filePath) {
+            console.log("Please pass the 'filePath' as input to this function!");
+            return null;
+        }
+
+        // Upload the file
+        const response = await cloudinary.uploader.upload(filePath, { resource_type: "auto" });
+        console.log("File Successfully Uploaded \nResponse: " + response);
+        return result;
+    } catch (error) {
+        fs.unlinkSync(filePath); // removes/deletes the locally stored file (in "/public/temp")
+        console.error("Error in Uploading Image: " + error);
+        return null;
+    }
+};
+
+export { uploadAssetToCloudinary };
