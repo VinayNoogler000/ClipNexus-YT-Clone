fcd7dad642ebbccab7f5d8fec1b3e0a68dee6b03
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-28 14:58:18 +0530

    Add Multer Middleware to User Registration Route for handling "multipart/form-data" requests, allowing users to upload both textual-data and files (their avatar and cover image).

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { registeredUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.post("/register", registeredUser);  
+const uploadMiddleware = upload.fields([ {name: "avatar", maxCount: 1}, {name: "coverImage", maxCount: 1} ]);
+router.post("/register", uploadMiddleware, registeredUser);  
 
 export default router;
