45b29cbe011da2e411349992c33dee78042c4e06
VinayNoogler000 <vinaytambey000@gmail.com>
2026-02-28 19:05:02 +0530

    Refractor Multer Middleware file to use ES modules syntax (by using import.meta.url, fileURLToPath, and dirname) for resolving the directory path where the uploaded files will be stored

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,9 +1,13 @@
 import multer from "multer";
-import path from "path";
+import { fileURLToPath } from 'url';
+import {join, dirname} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, "../../public/temp/"));
+        cb(null, join(__dirname, "../../public/temp/"));
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
