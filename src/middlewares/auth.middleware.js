c48632a2e6dccd20eedd42983f9216dd52ffdee2
VinayNoogler000 <vinaytambey000@gmail.com>
2026-03-05 13:35:05 +0530

    Add authentication middleware to verify JWT tokens in client-requests and protect routes from unauthorized access.

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import { jwt } from "jsonwebtoken";
+import ApiError from "../utils/ApiError";
+import asyncHandler from "../utils/asyncHandler";
+import { User } from "../models/user.model";
+
+export default verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorizaton")?.replace("Bearer ", "");
+    
+        if (!token) throw new ApiError(401, "Unauthorized Request!");
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+        const userInDB = await User.findById(decodedToken?._id).select("-password -refreshToken");
+    
+        if (!userInDB) throw new ApiError(401, "Invalid Access Token!");
+    
+        req.user = userInDB;
+        next();
+    } 
+    catch (error) {
+        throw new ApiError(401, error?.message || "Invalid Access Token!");
+    }
+});
