28460bbb1a9d94606ba82709fc29539684590853
VinayNoogler000 <vinaytambey000@gmail.com>
2026-03-12 11:46:18 +0530

    Add more information in the documentaiton of the subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -8,7 +8,7 @@ not an array of users. So, whenever we want to find the no. of subscribers of a 
 we just need to find those documents in this model whose "channel" property matches with the name (or _id)
 of the channel, and the count of those matched documents will give us the number of subscribers, and to
 know the count of channels a user has subscribed to, for that we need to find those docs in this model whose
-"subscriber" property matches with the name (or _id) of the user and the count of those docs will be exactly the number of channels the user has subscribed to. */
+"subscriber" property matches with the name (or _id) of the user and the count of those docs will be exactly the number of channels the user has subscribed to. A new document is created and added to the "subscriptions" collection whenever any user or channel subscribed to a different user/channel. */
 
 const subscriptionSchema = new Schema({
     subscriber: {
