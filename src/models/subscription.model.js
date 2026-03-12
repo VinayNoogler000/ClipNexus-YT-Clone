674395b9f45d0f43f1b963ecd7b73799a6ebd1ec
VinayNoogler000 <vinaytambey000@gmail.com>
2026-03-12 11:44:29 +0530

    Add documentation for Subscription Model to clarify its structure and usage.

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,12 +1,22 @@
 import { Schema, model } from "mongoose";
 
+/* The Subscription Model will be storing subscriptions as documents, which will consists of two main properties
+"subscriber" and "channel", where both are technically belong to "Users" model, but with different names, as
+"subscriber" will be referring to that user who has subscribed to a channel, whose name is stored "channel"
+property, means both the properties will be referring to single document (object_id) of the Users model, and
+not an array of users. So, whenever we want to find the no. of subscribers of a channel (internally an user),
+we just need to find those documents in this model whose "channel" property matches with the name (or _id)
+of the channel, and the count of those matched documents will give us the number of subscribers, and to
+know the count of channels a user has subscribed to, for that we need to find those docs in this model whose
+"subscriber" property matches with the name (or _id) of the user and the count of those docs will be exactly the number of channels the user has subscribed to. */
+
 const subscriptionSchema = new Schema({
-    subscriber: { // other users who've subscribed to the main user
+    subscriber: {
         type: Schema.Types.ObjectId,
         ref: "User",
         required: true
     },
-    channel: { // other users/channels who the main user has subscribed to
+    channel: { 
         type: Schema.Types.ObjectId,
         ref: "User",
         required: true
