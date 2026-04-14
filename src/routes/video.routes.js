ac1502f7fb53e591e1b1e237b8fce265180e67ea
VinayNoogler000 <vinaytambey000@gmail.com>
2026-04-14 21:08:42 +0530

    Add routes for CRUD in videos, and add signatures of controller functions for videos, which are yet to be defined.

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { deleteVideo, getAllVideos, getVideoById, publishVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller";
 
 const router = Router();
 
-
+router.get("/", getAllVideos);
+router.post("/", verifyJWT, publishVideo);
+router.get("/:videoId", getVideoById);
+router.patch("/:videoId", verifyJWT, updateVideo);
+router.delete("/:videoId", verifyJWT, deleteVideo);
+router.patch("/:videoId/publish-status", verifyJWT, togglePublishStatus);
 
 export default router;
