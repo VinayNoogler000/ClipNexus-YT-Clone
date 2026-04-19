76e42b24771971da557b9357ea5bb6d99e98ea98
VinayNoogler000 <vinaytambey000@gmail.com>
2026-04-19 19:45:29 +0530

    Refractor 'registeredUser' controller to use 'attemptFileUpload' and 'deleteTempFiles' utils for better error handling and cleanup of temporary files.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,34 +1,43 @@
-import { ApiError, ApiResponse, asyncHandler, uploadAssetToCloudinary, deleteAssetFromCloudinary, genRefreshAndAccessTokens, getImgPublicIdUsingURLSync } from "../utils/index.js"
+import { ApiError, ApiResponse, asyncHandler, uploadAssetToCloudinary, deleteAssetFromCloudinary, genRefreshAndAccessTokens, getImgPublicIdUsingURLSync, deleteTempFiles, attemptFileUpload } from "../utils/index.js"
 import { User } from "../models/user.model.js";
 import jwt from 'jsonwebtoken'
 import mongoose from "mongoose";
+import fs from "fs";
 
 const registeredUser = asyncHandler(async (req, res, err) => {
     // Extract the textual-data from 'req.body' and files/images from 'req.fles', then store it in proper & semantic variables
     const {userName, email, password, fullName} = req.body;
     const {avatar, coverImage} = req.files ? req.files : {};
+    const avatarLocalPath = avatar ? avatar[0].path : null;
+    const coverImgLocalPath = coverImage ? coverImage[0].path : null;
 
     // Validation - Check whether client has sent all the required data to create a new user or not?
     if ([userName, email, password, fullName].some(field => (!field || field.trim() === ""))) {
+        // remove the unnecessary temporary files (avatar & coverImage) from the server's directory
+        deleteTempFiles([avatarLocalPath, coverImgLocalPath]);
         throw new ApiError(400, "Missing Required User Data!");
     }
 
     // Check if the user already exists or not, by using Username or Email? if exists, then redirect to "/login" or else continue registration process
     const userFound = await User.exists({ $or: [{ userName: userName }, { email: email }] });
     if (userFound !== null) { // User Already Exists
+        deleteTempFiles([avatarLocalPath, coverImgLocalPath]);
         throw new ApiError(409, "User with Username/Email Already Exists! Either Log-In with the existing account, or Sign-Up with a different new account.");
     }
 
     // Validation and Upload the Avatar and Cover Image (if exists) to the Cloudinary's Server and Get their URL
-    if (!avatar) throw new ApiError(400, "User's Avatar doesn't exists [Required]");
-    
-    const avatarLocalPath =  avatar[0].path;
-    const coverImgLocalPath = coverImage ? coverImage[0].path : null;
-    
-    const uploadedAvatar = await uploadAssetToCloudinary(avatarLocalPath);
-    const uploadedCoverImg = coverImgLocalPath && (await uploadAssetToCloudinary(coverImgLocalPath));
+    if (!avatar) {
+        deleteTempFiles([coverImgLocalPath]);
+        throw new ApiError(400, "User's Avatar doesn't exists [Required]");
+    }
 
-    if (!uploadedAvatar) throw new ApiError(500, "Error in Uploading User Avatar to Cloud!");
+    const [uploadAvatarRes] = await attemptFileUpload(avatarLocalPath);
+    const [uploadCoverImgRes] = await attemptFileUpload(coverImgLocalPath);
+
+    if (!uploadAvatarRes) {
+        deleteTempFiles([avatarLocalPath, coverImgLocalPath]);    
+        throw new ApiError(500, "Error in Uploading User Avatar to Cloud!");
+    }
 
     // Add the user to the DB "users" collection as an Object/Document, whose avatar and coverImage will be Cloudinary-URLs
     const newUser = await User.create({
@@ -36,8 +45,8 @@ const registeredUser = asyncHandler(async (req, res, err) => {
         email,
         password,
         fullName,
-        avatar: uploadedAvatar.secure_url,
-        coverImage: uploadedCoverImg?.secure_url || ""
+        avatar: uploadAvatarRes.secure_url,
+        coverImage: uploadCoverImgRes?.secure_url || ""
     });
     
     // Check if the New User has successfully created/stored in the DB or not? If Yes, then send a response (without password and refresh token), and if No, then identify-fix the issue and repeat the process.
