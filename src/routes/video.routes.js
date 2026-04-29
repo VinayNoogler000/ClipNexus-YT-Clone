ccb14761e77d2cec24218adbbaa3bf31281c09b5
VinayNoogler000 <vinaytambey000@gmail.com>
2026-04-29 17:13:52 +0530

    Refractor "Publish-Video" Route to handle not only Video uploads, but also Thumbnail Uploads.

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middleware";
-import { deleteVideo, getAllVideos, getVideoById, publishVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { deleteVideo, getAllVideos, getVideoById, publishVideo, togglePublishStatus, updateVideo } from "../controllers/video.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.get("/", getAllVideos);
-router.post("/", verifyJWT, publishVideo);
+router.post("/", verifyJWT, 
+    upload.fields([ 
+        { name: "videoFile", maxCount: 1 },
+        { name: "thumbnail", maxCount: 1 } 
+    ]), 
+    publishVideo);
 router.get("/:videoId", getVideoById);
 router.patch("/:videoId", verifyJWT, updateVideo);
 router.delete("/:videoId", verifyJWT, deleteVideo);
